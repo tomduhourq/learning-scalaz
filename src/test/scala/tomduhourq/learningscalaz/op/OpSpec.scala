4ca6dd75b49f2ea3eb803d1f04cc2ca221bd7207
tomduhourq <tomas.duhourq@avenida.com>
2015-10-16 17:43:54 -0300

    Remove OptionOp instance

--- src/test/scala/tomduhourq/learningscalaz/op/OpSpec.scala
+++ src/test/scala/tomduhourq/learningscalaz/op/OpSpec.scala
@@ -3,7 +3,6 @@ package tomduhourq.learningscalaz.op
 import org.scalatest.FlatSpec
 import tomduhourq.learningscalaz.monoid.Monoid._
 import tomduhourq.learningscalaz.op.MonoidOp._
-//import tomduhourq.learningscalaz.op.OptionOp._
 
 class OpSpec extends FlatSpec {
 
@@ -17,10 +16,4 @@ class OpSpec extends FlatSpec {
     // specifying its List type.
     assert((List(1, 2, 3) |+| List(2) |+| Nil |+| List(4, 5)) == List(1, 2, 3, 2, 4, 5))
   }
-
-//  "OptionOp" should "return correct results" in {
-//    assert(1.some == Some(1))
-//    assert((2 | 3) == 2)
-//  }
-
 }
